0aab973a490745800909a0b6230ef87b05633708
Michel Massamiri <mmassamire@services.emi.u-bordeaux.fr>
2018-04-10 13:12:15 +0200

    Implementing concrete class for UI builder

---
+++ src/view/JavaFXBuilder.java
@@ -0,0 +1,33 @@
+package view;
+
+import javafx.scene.canvas.*;
+import javafx.scene.control.*;
+import javafx.scene.Scene;
+
+public class JavaFXBuilder implements UIBuilder {
+	private Canvas canvas;
+	private ToolBar toolbar;
+	private MenuBar menuBar;
+	private Menu menu;
+	private Scene scene;
+	
+	public JavaFXBuilder() {
+
+	}
+	
+	public void buildMenu() {
+		
+	}
+	
+	public void buildToolbar() {
+		
+	}
+	
+	public void buildCanvas() {
+		
+	}
+	
+	public Scene getResult() {
+		return scene;
+	}
+}
