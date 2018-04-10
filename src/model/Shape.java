e8c7485314ce235583f4505947ef19ffd0cb506f
kfontain <fontaken@gmail.com>
2018-04-10 12:13:37 +0200

    Create model/shape

---
+++ src/model/Shape.java
@@ -0,0 +1,22 @@
+package model;
+
+public interface Shape {
+
+    Position getPosition();
+
+
+    class Position {
+        private int x;
+        private int y;
+
+        public Position() {
+            x = y = 0;
+        }
+
+        public int getY() { return y; }
+        public int getX() { return x; }
+        public void setX(int n) { x = n; }
+        public void setY(int n) { y = n; }
+    }
+
+}
