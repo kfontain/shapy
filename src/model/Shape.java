863ff572ed03bb7ce0148d1247b817d81826734d
kfontain <fontaken@gmail.com>
2018-04-13 14:52:28 +0200

    Update Shape interface

--- src/model/Shape.java
+++ src/model/Shape.java
@@ -2,6 +2,23 @@ package model;
 
 public interface Shape {
 
+    /* position parameter */
     Position getPosition();
+    void translate();
 
+    int getRotation();
+    void setRotation(int r);
+
+    Position getRotationCenter();
+    void setRotationCenter(Position p);
+
+    Position getTranslationCenter();
+    void setTranslationCenter(Position p);
+
+    RGB getRGB();
+    void setRGB(RGB rgb);
+
+    int getArrondi();
+    void setArrondi(int a);
+    
 }
