2cc5740470afb87a19a5490ced51dc320c93a9c0
kfontain <fontaken@gmail.com>
2018-04-13 14:41:49 +0200

    Refactor Position

--- src/model/Position.java
+++ src/model/Position.java
@@ -2,16 +2,17 @@ package model;
 
 public class Position {
 
-    private int x;
-    private int y;
+    private double x;
+    private double y;
 
     public Position() {
            x = y = 0;
     }
 
-    public int getY() { return y; }
-    public int getX() { return x; }
+    public double getX() { return x; }
     public void setX(int n) { x = n; }
+
+    public double getY() { return y; }
     public void setY(int n) { y = n; }
 
 }
