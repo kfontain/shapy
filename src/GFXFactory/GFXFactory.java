6d8044f7d188f1d7a4e549823e3885bbcaf06382
Michel Massamiri <mmassamire@services.emi.u-bordeaux.fr>
2018-04-19 11:51:00 +0200

    Factory will be in charge to create the controller depending on GUI

--- src/GFXFactory/GFXFactory.java
+++ src/GFXFactory/GFXFactory.java
@@ -1,9 +1,7 @@
 package GFXFactory;
 
 import controller.Controller;
-import view.View;
 
 public interface GFXFactory {
 	public Controller createController();
-	public View createView();
 }
--- src/GFXFactory/JavaFXFactory.java
+++ src/GFXFactory/JavaFXFactory.java
@@ -3,20 +3,16 @@ package GFXFactory;
 import controller.Controller;
 import controller.JavaFXController;
 import view.JavaFXView;
-import view.View;
 
 public class JavaFXFactory implements GFXFactory{
 	/* Attributes will be here */
+	private JavaFXView view;
 	
 	public JavaFXFactory() {
-		
+		view = new JavaFXView();
 	}
 	
 	public Controller createController() {
-		return new JavaFXController();
-	}
-	
-	public View createView() {
-		return new JavaFXView();
+		return new JavaFXController(view);
 	}
 }
